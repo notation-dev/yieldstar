454b2ea8634ce84bf7fd260e9ed70e38a90dc98b
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2024-10-05 14:59:42 -0700

    Remove in-memory example

--- examples/in-memory/router.ts
+++
@@ -1,8 +0,0 @@
-import { createWorkflowRouter } from "yieldstar";
-import { simpleWorkflow } from "../workflows/simple";
-import { pollingWorkflow } from "../workflows/polling";
-
-export const workflowRouter = createWorkflowRouter({
-  "simple-workflow": simpleWorkflow,
-  polling: pollingWorkflow,
-});
