511f68b33f452acb941caa8e8618667df17e0d04
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2025-03-07 09:41:22 +0000

    Pass logger through to invoker

--- packages/bun-sqlite-runtime/src/sqlite-event-loop.ts
+++ packages/bun-sqlite-runtime/src/sqlite-event-loop.ts
@@ -2,6 +2,7 @@ import { Database } from "bun:sqlite";
 import { SqliteTaskQueue } from "./sqlite-task-queue";
 import { SqliteTimers } from "./sqlite-timers";
 import type { TaskProcessor } from "@yieldstar/core";
+import type { Logger } from "pino";
 
 export class SqliteEventLoop {
   taskQueue: SqliteTaskQueue;
@@ -13,28 +14,28 @@ export class SqliteEventLoop {
     this.timers = new SqliteTimers({ db, taskQueue: this.taskQueue });
   }
 
-  start(params: { onNewTask: TaskProcessor }) {
+  start(params: { onNewTask: TaskProcessor; logger: Logger }) {
     this.isRunning = true;
-    this.loop(params.onNewTask);
+    this.loop(params.onNewTask, params.logger);
   }
 
   stop() {
     this.isRunning = false;
   }
 
-  private async loop(processTask: TaskProcessor) {
+  private async loop(processTask: TaskProcessor, logger: Logger) {
     if (!this.isRunning) return;
 
     while (!this.taskQueue.isEmpty) {
       const task = this.taskQueue.process();
       if (task) {
-        await processTask(task);
+        await processTask(task, logger);
         this.taskQueue.remove(task.taskId);
       }
     }
 
     this.timers.processTimers();
 
-    setTimeout(() => this.loop(processTask), 10);
+    setTimeout(() => this.loop(processTask, logger), 10);
   }
 }
