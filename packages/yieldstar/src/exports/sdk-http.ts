bf4126a70ad22c4324ade8296d184918b36ce803
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2025-03-11 15:20:41 +0000

    Support triggering workflows in browser environments

--- packages/yieldstar/src/exports/sdk-http.ts
+++ packages/yieldstar/src/exports/sdk-http.ts
@@ -4,7 +4,7 @@ import type {
   TriggerEvent,
   ExecutionEvent,
 } from "@yieldstar/core";
-import { randomUUID } from "node:crypto";
+import { nanoid } from "nanoid";
 import { deserializeError, isErrorLike } from "serialize-error";
 import { errorWithOriginalStack } from "../internal/serialise";
 import type { TriggerAck, WorkflowResult } from "../internal/types";
@@ -16,7 +16,7 @@ export function createHttpSdkFactory<W extends WorkflowRouter>() {
         event: TriggerEvent<K, EventParamsOf<W[K]>>
       ): TriggerAck {
         const executionEvent: ExecutionEvent = {
-          executionId: event?.executionId ?? randomUUID(),
+          executionId: event?.executionId ?? nanoid(),
           workflowId: event?.workflowId as string,
           params: event?.params,
         };
--- packages/yieldstar/src/exports/sdk-local.ts
+++ packages/yieldstar/src/exports/sdk-local.ts
@@ -5,7 +5,7 @@ import type {
   TriggerEvent,
   ExecutionEvent,
 } from "@yieldstar/core";
-import { randomUUID } from "node:crypto";
+import { nanoid } from "nanoid";
 import type { TriggerAck, WorkflowResult } from "../internal/types";
 
 export function createLocalSdk<W extends WorkflowRouter>(
@@ -16,7 +16,7 @@ export function createLocalSdk<W extends WorkflowRouter>(
       event: TriggerEvent<K, EventParamsOf<W[K]>>
     ): TriggerAck {
       const executionEvent: ExecutionEvent = {
-        executionId: event.executionId ?? randomUUID(),
+        executionId: event.executionId ?? nanoid(),
         workflowId: event.workflowId,
         params: event.params,
       };
@@ -26,7 +26,7 @@ export function createLocalSdk<W extends WorkflowRouter>(
     async triggerAndWait<K extends string & keyof W>(
       event: TriggerEvent<K, EventParamsOf<W[K]>>
     ): WorkflowResult<W, K> {
-      const executionId = event.executionId ?? randomUUID();
+      const executionId = event.executionId ?? nanoid();
       const workflowCompletePromise = new Promise((resolve) => {
         invoker.workflowEndEmitter.once(executionId, resolve);
       });
